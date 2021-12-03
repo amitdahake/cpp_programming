32d09cfd49470952e757cebdfbf6e366ab23860b
Amit Dahake <92622826+amitdahake@users.noreply.github.com>
2021-12-03 14:34:15 +0530

    Create assign_function_3_addOfTwoNumberUsingFunction.cpp

---
+++ assign_function_3_addOfTwoNumberUsingFunction.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+
+addition(int x,int y){
+int add = x+y;
+cout<<"addition of 5 + 8: "<<add;
+}
+int main(){
+    int a = 5;
+    int b = 8;
+    addition(a,b);
+
+return 0;
+}
